59cc722c028ba8b18a8f735d40077f882ec1572a
egertonmaciel <egertonmaciel@INFO014w.vigsc.priv.ds>
2018-03-21 17:56:13 -0300

    MODAL FORM FICHA

--- web/js/app.js
+++ web/js/app.js
@@ -74,8 +74,8 @@ angular.module("crudFichas", []).controller("controller", function ($scope, $htt
         });
     };
 
-    $scope.excluirFicha = function (ficha) {
-        $scope.url = '/CrudFichas/webresources/ficha/excluir/' + ficha.id;
+    $scope.excluirFicha = function () {
+        $scope.url = '/CrudFichas/webresources/ficha/excluir/' + $scope.ficha.id;
         $http({
             url: $scope.url,
             method: 'GET'
@@ -83,6 +83,12 @@ angular.module("crudFichas", []).controller("controller", function ($scope, $htt
             $scope.carregarFichas();
         });
     };
+    
+    $scope.confirmaExcluirFicha = function (ficha) {
+        console.log(ficha)
+        $scope.ficha = ficha;
+    };
+    
 
     $scope.novaFicha = function () {
         $scope.zeraFicha();
@@ -133,7 +139,7 @@ angular.module("crudFichas", []).controller("controller", function ($scope, $htt
         });
     };
 
-    $scope.deletarAnimal = function () {
+    $scope.excluirAnimal = function () {
         $scope.url = '/CrudFichas/webresources/animal/excluir/';
         $http.post($scope.url, $scope.animal, $scope.retorno).then(
                 function (response) {
@@ -158,9 +164,10 @@ angular.module("crudFichas", []).controller("controller", function ($scope, $htt
         $scope.animal.nome = animal.nome;
     };
     
-    $scope.confirmaDeletarAnimal = function (animal) {
-        $scope.animal.id = animal.id;
-        $scope.animal.nome = animal.nome;
+    $scope.confirmaExcluirAnimal = function (animal) {
+//        $scope.animal.id = animal.id;
+//        $scope.animal.nome = animal.nome;
+        $scope.animal = animal;
     };
     $scope.zeraAnimal();
     $scope.carregarAnimais();
